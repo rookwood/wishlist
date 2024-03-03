fe4ff0d40316bd2b04e9063cb9a260b7fd9939ab
Matt Hollis <matthollis@gmail.com>
2024-03-03 17:17:30 -0600

    Add missing return types

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Item extends Model
 {
@@ -22,22 +24,22 @@ class Item extends Model
         ]);
     }
 
-    public function quantityPurchased()
+    public function quantityPurchased(): int
     {
         return $this->purchases->count();
     }
 
-    public function purchases()
+    public function purchases(): HasMany
     {
         return $this->hasMany(Purchase::class);
     }
 
-    public function stillneeds()
+    public function stillneeds(): int
     {
         return $this->quantity - $this->quantityPurchased();
     }
 
-    public function wishlist()
+    public function wishlist(): BelongsTo
     {
         return $this->belongsTo(Wishlist::class);
     }
--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -6,17 +6,19 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Wishlist extends Model
 {
     use HasFactory, HasUlids;
 
-    public function items()
+    public function items(): HasMany
     {
         return $this->hasMany(Item::class);
     }
 
-    public function users()
+    public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
     }
