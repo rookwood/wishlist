5c408faf548f19497fa112193f0c7a456469c8d1
Matt Hollis <matthollis@gmail.com>
2024-03-03 13:57:21 -0600

    Improve readability in dev UI

--- resources/views/wishlists/show.blade.php
+++ resources/views/wishlists/show.blade.php
@@ -3,26 +3,26 @@
         <h1 class="text-xl mb-4">{{ $wishlist->name }}</h1>
         <h3 class="text-md text-gray-500 mb-12">By {{ $wishlist->owners }}</h3>
     </div>
-    <ul>
+    <ul class="divide-gray-300 divide-y">
         @foreach($wishlist->items as $item)
-            <li class="mb-8">
-                <div>
+            <li class="my-4">
+                <div class="my-2">
                     <a href="{{ $item->url }}" class="flex justify-between">
-                        <span>{{ $item->name }}</span>
-                        <span>{{ $item->price }}</span>
+                        <span class="text-lg">{{ $item->name }}</span>
+                        <span class="text-gray-600">{{ $item->price }}</span>
                     </a>
                 </div>
                 <div class="flex justify-between">
                     <div>
-                        <span>{{ $item->notes }}</span>
+                        <span class="text-gray-600">{{ $item->notes }}</span>
                     </div>
-                    <div>
-                        <span>Wants {{ $item->quantity }}</span>
+                    <div class="flex flex-col">
+                        <span class="w-16">Wants {{ $item->quantity }}</span>
                         <span>Has ???</span>
                     </div>
                 </div>
                 <div>
-                    <span>{{ $item->created_at }}</span>
+                    <span class="text-gray-400">Added on {{ $item->created_at->toFormattedDateString() }}</span>
                 </div>
             </li>
         @endforeach
